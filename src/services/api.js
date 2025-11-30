c7ee53fa22b568f6e71e8fa96db2bab05048b349
uma <udayarekhachowdary96@gmail.com>
2025-11-30 12:35:52 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "https://backend-pasovit.onrender.com";
 
 async function request(
   path,
